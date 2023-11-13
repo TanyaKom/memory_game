cbde9b94efc9c2a7ae07cf1d7eed24f02041b559
TanyaKom <141914090+TanyaKom@users.noreply.github.com>
2023-11-13 22:36:02 +0200

    updated game

--- script.js
+++ script.js
@@ -61,6 +61,89 @@ function blockFirstSecondCards() {
 	firstTurnedCard.removeEventListener("click", onClickedCard);
 	secondTurnedCard.removeEventListener("click", onClickedCard);
 	resetBoard();
+	checkAllCardsOpened();
 }
 
 initializeBoard();
+
+
+
+let timerDisplay = document.getElementById("timer");
+let startButton = document.getElementById("startButton");
+let gameContainer = document.getElementById("game-container");
+
+let totalSeconds = 60;
+let interval;
+
+function startTimer() {
+interval = setInterval(() => {
+	if (totalSeconds <= 0) {
+		clearInterval(interval);
+
+		if(!checkAllCardsOpened()) {
+			let loseWindow = document.getElementById("lose game");
+			loseWindow.style.display = "flex";
+		}
+	} else {
+		
+		let minutes = Math.floor(totalSeconds / 60);
+		let seconds = totalSeconds % 60;
+
+	if (seconds < 10) {
+		seconds = `0${seconds}`;
+	}
+		timerDisplay.innerHTML = `${minutes}:${seconds}`;
+		totalSeconds--;
+		}
+	},1000);
+}
+
+function startGame() {
+	let background = document.getElementById("game-container");
+
+	document.querySelector(".memory-game").style.setProperty("display", "flex");
+
+	background.classList.remove("initial-bckg");
+	background.classList.add("body");
+
+	let allCards = document.querySelectorAll(".memory-card");
+	allCards.forEach(card => {
+		card.style.display = "block";
+	});
+	startTimer();
+}
+
+
+document.getElementById("startButton").addEventListener("click",function() {
+	document.querySelector(".start-page").style.display = "none";
+	document.querySelector(".memory-game").style.setProperty("display", "flex");
+
+	startGame();	
+});
+
+function checkAllCardsOpened() {
+let allCardsOpened = document.querySelectorAll(".memory-card");
+let countOpened = 0;
+
+allCardsOpened.forEach((card) => {
+	if (card.classList.contains("flip")) {
+		countOpened++;
+		}
+});
+if (countOpened === allCardsOpened.length) {
+	let openWindow = document.getElementById("finishWindow");	
+	
+	openWindow.style.display = "flex";
+	return true;
+
+}
+return false;
+}
+
+function playAgain() {
+	let openWindow = document.getElementById("finishWindow");
+	let loseWindow = document.getElementById("lose game");
+	openWindow.style.display = "none";
+	loseWindow.style.display = "none";
+	location.reload();
+}
